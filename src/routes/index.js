4750a8e4bad10e44f03744285a30dd0624822034
Ezequiel Rango <ezequielrango29@gmail.com>
2022-07-19 23:35:25 -0300

    :sparkles: create index routes

---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const storesRouter = require('./stores_router');
+
+router.use('/stores', storesRouter);
+
+module.exports = router ;
