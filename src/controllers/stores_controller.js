6df44af42c4cdf13e10f869e0c173167bdd96844
Ezequiel Rango <ezequielrango29@gmail.com>
2022-07-20 02:53:02 -0300

    :construction: create GetById controller_Store

--- src/controllers/stores_controller.js
+++ src/controllers/stores_controller.js
@@ -1,8 +1,8 @@
 const mysqlConnection = require('../database/database');
 
 
-const getAll = async (req,res) => {
-     mysqlConnection.query("SELECT * FROM stores", async (err, rows,fields) => {
+const getAll = async (req, res) => {
+    mysqlConnection.query("SELECT * FROM stores", async (err, rows, fields) => {
         try {
             if (!rows) {
                 console.log(err);
@@ -10,7 +10,25 @@ const getAll = async (req,res) => {
             } else {
                 const data = await rows;
                 res.status(200).json(data)
-            };         
+            };
+        } catch (err) {
+            console.log(err);
+            res.status(500).json('Internal Server Error')
+        }
+    });
+};
+
+const getById = async (req, res) => {
+    const {id} = req.params;
+    mysqlConnection.query("SELECT * FROM stores WHERE id= ?",id, async (err, rows, fields) => {
+        try {
+            if (!rows) {
+                console.log(err);
+                res.status(404).msg('Resource doesnt exist')
+            } else {
+                const data = await rows;
+                res.status(200).json(data)
+            };
         } catch (err) {
             console.log(err);
             res.status(500).json('Internal Server Error')
