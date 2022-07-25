12c70cc146915c476089239227a478d2a4ca6207
Ezequiel Rango <ezequielrango29@gmail.com>
2022-07-25 16:28:07 -0300

    :construction: create getAll & getById staff contreollers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const storesRouter = require('./stores_router');
+const staffRouter = require('./staff_router');
 
 router.use('/stores', storesRouter);
+router.use('/staff', staffRouter);
 
 
 module.exports = router ;
